678c7128ff981224d045909e74de37368d7b939b
Alexander Stehlik <alexander.stehlik@gmail.com>
2015-07-27 17:51:40 +0200

    [TASK] Cleanup ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,48 +1,22 @@
 <?php
-
-########################################################################
-# Extension Manager/Repository config file for ext "webkitpdf".
-#
-# Auto generated 25-05-2010 19:04
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Webkit PDFs',
 	'description' => 'Generate PDF files using WebKit rendering engine.',
 	'category' => 'plugin',
-	'shy' => 0,
-	'version' => '1.2.0',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
 	'state' => 'beta',
 	'uploadfolder' => 0,
 	'createDirs' => 'typo3temp/tx_webkitpdf/',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
+	'clearCacheOnLoad' => 0,
 	'author' => 'Dev-Team Typoheads',
 	'author_email' => 'dev@typoheads.at',
 	'author_company' => 'Typoheads GmbH',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'version' => '2.0.0-dev',
+	'_md5_values_when_last_written' => '',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '6.2.3-7.99.99'
 		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => 'a:16:{s:21:"ext_conf_template.txt";s:4:"d376";s:12:"ext_icon.gif";s:4:"f735";s:17:"ext_localconf.php";s:4:"01c8";s:14:"ext_tables.php";s:4:"4310";s:14:"ext_tables.sql";s:4:"2937";s:16:"locallang_db.xml";s:4:"5127";s:14:"doc/manual.pdf";s:4:"d81a";s:14:"doc/manual.sxw";s:4:"fbe7";s:30:"pi1/class.tx_webkitpdf_pi1.php";s:4:"634f";s:17:"pi1/locallang.xml";s:4:"3153";s:32:"res/class.tx_webkitpdf_cache.php";s:4:"12b7";s:32:"res/class.tx_webkitpdf_utils.php";s:4:"6f3d";s:22:"res/user_webkitpdf.php";s:4:"438e";s:15:"res/wkhtmltopdf";s:4:"3774";s:20:"static/constants.txt";s:4:"1701";s:16:"static/setup.txt";s:4:"8e66";}',
-	'suggests' => array(
+		'conflicts' => array(),
+		'suggests' => array(),
 	),
 );
-
-?>
