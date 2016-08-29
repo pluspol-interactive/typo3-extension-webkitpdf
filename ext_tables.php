c53531575f1028d093e784df4f04a215d9036242
Alexander Stehlik <alexander.stehlik@gmail.com>
2016-08-29 10:33:46 +0200

    [BUGFIX] Remove use statement from ext_tables.php
    use statements within ext_tables.php / ext_localconf.php
    files can cause problematic side effects and fatal errors.
    Do not use them!

--- ext_tables.php
+++ ext_tables.php
@@ -1,15 +1,17 @@
 <?php
-
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+/* @var string $_EXTKEY */
 
 // Add static file for plugin
-ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'WebKit PDF');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'WebKit PDF');
 
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,pages,select_key';
 
-ExtensionManagementUtility::addPlugin(array('LLL:EXT:webkitpdf/Resource/Private/Language/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY . '_pi1'), 'list_type');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    ['LLL:EXT:webkitpdf/Resource/Private/Language/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY . '_pi1'],
+    'list_type'
+);
 
