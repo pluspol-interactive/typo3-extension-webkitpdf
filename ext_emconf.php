14e63465f164469055175242ed24998e31ce73bf
Alexander Opitz <opitz@pluspol.info>
2019-05-08 13:09:12 +0200

    Mark this as compatible to v8LTS and v9LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,22 +1,19 @@
 <?php
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Webkit PDFs',
-	'description' => 'Generate PDF files using WebKit rendering engine.',
-	'category' => 'plugin',
-	'state' => 'beta',
-	'uploadfolder' => 0,
-	'createDirs' => 'typo3temp/tx_webkitpdf/',
-	'clearCacheOnLoad' => 0,
-	'author' => 'Dev-Team Typoheads',
-	'author_email' => 'dev@typoheads.at',
-	'author_company' => 'Typoheads GmbH',
-	'version' => '2.0.0-dev',
-	'_md5_values_when_last_written' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.3-8.7.99'
-		),
-		'conflicts' => array(),
-		'suggests' => array(),
-	),
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Webkit PDFs',
+    'description' => 'Generate PDF files using WebKit rendering engine.',
+    'category' => 'plugin',
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'clearCacheOnLoad' => 0,
+    'author' => 'Dev-Team Typoheads',
+    'author_email' => 'dev@typoheads.at',
+    'author_company' => 'Typoheads GmbH',
+    'version' => '2.1.0-dev',
+    '_md5_values_when_last_written' => '',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-9.5.99',
+        ],
+    ],
+];
